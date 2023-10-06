61594d588f0e036522d6aed8e2dbf8e5322e6597
Ehsan Jalilifar <ehsan.jalilifar@gmail.com>
2023-10-06 12:42:12 -0500

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Ehsan Jalilifar on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
